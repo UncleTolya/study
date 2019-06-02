1807668075935e9771d8a1a67430c3cf3d82c948
uncletolya <stolikzakazan@gmail.com>
2019-06-02 12:34:49 +0300

    database study

--- src/main/mysqlstudy/DBTest.java
+++ src/main/mysqlstudy/DBTest.java
@@ -5,14 +5,22 @@ import main.junitstudy.persons.workers.Worker;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DBTest {
     static Connection connection;
+    static Statement statement;
 
     public static void main(String[] args) throws Exception {
         connection = DBConnection.getConnection();
-        Statement statement = connection.createStatement();
+        statement = connection.createStatement();
+        testStatementClass();
+        soutEmployeeOrderByLnameAndFname();
+
+    }
+
+    public static void testStatementClass() throws SQLException {
         String query = "select * from person";
         ResultSet set = statement.executeQuery(query);
         System.out.println("Select statement from DB has been complete");
@@ -25,5 +33,19 @@ public class DBTest {
         }
         worker.beingSevere();
         System.out.printf("%s, say: i am %d years old!", worker.getName(), worker.getAge());
+        System.out.println();
+    }
+
+    public static void soutEmployeeOrderByLnameAndFname() throws SQLException {
+        String query = "select emp_id, fname, lname from employee order by lname, fname";
+        ResultSet set = statement.executeQuery(query);
+        System.out.println("Select statement from DB has been complete");
+        while (set.next()) {
+            int id = set.getInt("emp_id");
+            String fname = set.getString("fname");
+            String lname = set.getString("lname");
+            System.out.println(id + "\t" + fname + "\t" + lname);
+        }
+        System.out.println();
     }
 }
